6e1b20d89961d1f9f1661aa7ebea347493667bec
Adam Dorogi-Kaposi <adam.dorogi@gmail.com>
2023-05-16 19:48:13 +0200

    Add logs

--- src/listeners/message.ts
+++ src/listeners/message.ts
@@ -2,13 +2,17 @@ import { Message, PartialMessage } from "discord.js"
 
 
 export const onMessageCreate = async (message: Message<boolean>) => {
+    console.log("New message created.");
     if (message.content.includes("discord.gg/") && message.author.id !== message.client.user.id) {
+        console.log("Message contains invite link, deleting...");
         await message.delete();
     }
 }
 
 export const onMessageUpdate = async (_: Message<boolean> | PartialMessage, newMessage: Message<boolean> | PartialMessage) => {
+    console.log("New message updated.");
     if (newMessage.content?.includes("discord.gg/") && newMessage.author?.id !== newMessage.client.user.id) {
+        console.log("Message contains invite link, deleting...");
         await newMessage.delete();
     }
 }
